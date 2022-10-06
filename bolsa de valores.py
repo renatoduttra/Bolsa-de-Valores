3fea56891fcb2b229fd48e84c9061790c968a71a
renatoduttra <renatoduttra@gmail.com>
2022-10-05 22:37:38 -0300

    Web Scraping comleto
    Extração dos valores concluida

--- bolsa de valores.py
+++ bolsa de valores.py
@@ -1,14 +1,33 @@
+import unicodedata
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
 with open("dadosteste.txt", "r") as arquivo: 
   dados = arquivo.read()
 
 dados= dados.split()
 print(dados)
 
+for i in range(len(dados)):
+    link = "https://www.google.com.br/search?q={}".format(dados[i])
+    print(link)
 
+    req = Request(link, headers={
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                        'Chrome/81.0.4044.138 Safari/537.36'})
 
-
-
-
+    webpage = urlopen(req).read()
+    #print(webpage)
+    preco =[]
+    with requests.Session() as c:
+        soup = BeautifulSoup(webpage, 'html.parser')
+        # print(soup)
+        for item in soup.find_all('div', attrs={'class': 'wGt0Bc'}):  # dbsr
+            valor = (item.find('span', attrs={'class': 'IsqQVc NprOob wT3VGc'}))
+            valor = valor.text
+            print(valor)
+            preco.append(valor)
 
 
 
